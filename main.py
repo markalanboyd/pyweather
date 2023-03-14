f22395b8c8b947d6b2da22a77e1885d6aa27ff3b
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-03-14 18:31:41 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# pyenv activate pyweather-3.11.2
+
+import pip._vendor.requests as requests
+
+API_URL = "https://api.openweathermap.org/data/2.8/onecall?lat=42.9297&lon=16.8886&appid=a8399132b50ffa51edbd9b19e1e3a8fa"
+
+forecast_response = requests.get(API_URL)
+forecast_response_code = forecast_response.status_code
+forecast_json = forecast_response.json()
+
+print(forecast_response_code)
+
+forecast_next_24 = dict(forecast_json["hourly"][0:25])
+
+for hour in forecast_next_24:
+    if forecast_next_24[hour]["weather"][0]["main"] == "Rain":
+        print("Rain's a-comin'")
+        break
+    else:
+        print("Clear skies")
