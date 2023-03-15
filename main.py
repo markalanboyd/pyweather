b5d43c7b5e2d868247238fb74313c37293e7a934
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-03-15 14:15:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,31 @@
 
 import pip._vendor.requests as requests
 
-API_URL = "https://api.openweathermap.org/data/2.8/onecall?lat=42.9297&lon=16.8886&appid=a8399132b50ffa51edbd9b19e1e3a8fa"
 
-forecast_response = requests.get(API_URL)
-forecast_response_code = forecast_response.status_code
+API_KEY = "a8399132b50ffa51edbd9b19e1e3a8fa"
+
+weather_params = {
+    "lat": 42.9297,
+    "lon": 16.8886,
+    "appid": API_KEY,
+    "exclude": "current,minutely,daily,alerts",
+}
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.8/onecall"
+
+forecast_response = requests.get(OWM_Endpoint,
+                                 params=weather_params)
+forecast_response.raise_for_status()
 forecast_json = forecast_response.json()
 
-forecast_next_24 = forecast_json["hourly"][0:25]
+forecast_next_12 = forecast_json["hourly"][0:12]
 
-for hour in range(0,25):
-    if forecast_next_24[hour]["weather"][0]["main"] == "Rain":
-        print("Rain's a-comin'")
-        break
-    else:
-        print("Clear skies")
+def check_for_rain(forecast):
+    for hour in range(0,12):
+        if forecast_next_12[hour]["weather"][0]["id"] < 700:
+            return True
+
+if check_for_rain(forecast_next_12):
+    print("Rain's a-comin'.")
+else:
+    print("Clear skies today.")
